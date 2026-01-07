47707ae6391c7b327aa729120f2b3e869cfc9b98
eande <eande@LAPTOP-ITKNUQMP>
2026-01-07 18:28:47 +0100

    Add project files.

---
+++ BallotBinMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BallotBinMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
