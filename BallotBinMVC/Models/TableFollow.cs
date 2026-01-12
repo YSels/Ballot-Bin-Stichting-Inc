9e7430d7d88bb392e24b73681476bf2131ce0cbe
ea433 <eandersen424@hotmail.com>
2026-01-12 11:43:57 +0100

    backend bestand met database connectie inbegrepen

---
+++ BallotBinMVC/Models/TableFollow.cs
@@ -0,0 +1,8 @@
+﻿namespace BallotBinMVC.Models
+{
+    public class TableFollow
+    {
+        public string FollowingUserId { get; set; } = null!;
+        public string FollowedUserId { get; set; } = null!;
+    }
+}
---
+++ BallotBinMVC/Models/TableUsercs.cs
@@ -0,0 +1,9 @@
+﻿namespace BallotBinMVC.Models
+{
+    public class TableUser
+    {
+        public string Id { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
